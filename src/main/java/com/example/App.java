b462d6b6693f1b28eeb8f067c5ee8d10dc28748f
gitlab <arohansingh300@gmail.com>
2025-05-25 16:21:05 +0530

    Hi

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven" );
+        System.out.println("This is a real world example....");
+        int a=5,b=10;
+        System.out.println("Sum of 5 and 10 is 15");
+    }
+}
